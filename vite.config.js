af93aefc0e430ed36476d8dbe31a87a6e76fa3d2
Massimiliano Scalia <massimiliano.sca@gmail.com>
2025-08-22 13:18:24 +0200

    prova per vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Forza Rollup a non esternalizzare nulla
+      external: id => false,
+    },
+  },
 })
